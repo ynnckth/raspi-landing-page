8123588125280d418540bf501907a91207ceb7c8
ynnck <o5YV;)YsA4,L6#D>
2023-06-24 16:17:26 +0800

    Write output file into static folder; ignore replaced index file from git

--- prepare.sh
+++ prepare.sh
@@ -3,6 +3,6 @@
 IP_ADDRESS=$(hostname -I | awk '{print $1}')
 echo "IP address is $IP_ADDRESS"
 
-sed "s/{{IP_ADDRESS}}/$IP_ADDRESS/g" template.html > index.html
+sed "s/{{IP_ADDRESS}}/$IP_ADDRESS/g" template.html > ./static/index.html
 
 echo "Created new file index.html"
