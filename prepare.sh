41a680987f3b88ee4068049710631652472d20f9
ynnck <o5YV;)YsA4,L6#D>
2023-06-24 19:03:37 +0800

    Update message

--- prepare.sh
+++ prepare.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 IP_ADDRESS=$(hostname -I | awk '{print $1}')
-echo "IP address is $IP_ADDRESS"
+echo "Your Pi's IP address is: $IP_ADDRESS"
 
 sed "s/{{IP_ADDRESS}}/$IP_ADDRESS/g" template.html > ./static/index.html
 
-echo "Created new file index.html"
+echo "Successfully generated landing page at ./static/index.html"
